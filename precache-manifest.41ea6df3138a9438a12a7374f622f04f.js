c5bd033e1ec912d6de1df750662ba6cdc974c173
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-09-02 18:45:16 +0200

    Updates

---
+++ precache-manifest.41ea6df3138a9438a12a7374f622f04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786a86597189119cfecd153ca634ff5a",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "9cc51a1d4ed1a991b4fb",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c03b1c7471d05d480398",
+    "url": "/test/static/js/2.29496ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.29496ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc51a1d4ed1a991b4fb",
+    "url": "/test/static/js/main.50a0939b.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
