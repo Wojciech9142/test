89d4bf3eceb3816eacc98d068ce2a8fe8a84109a
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-09-02 18:22:42 +0200

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+
+axios.get("products.json")
+  .then(response => console.log(response.data))
+  .catch(error => console.error(error));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello world!</h1>
   );
 }
 
